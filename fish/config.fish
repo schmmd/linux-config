eae4cafad7b7d302886ba275c4d45bf6f8bcf271
Michael Schmitz <michael@schmitztech.com>
2014-12-23 11:40:23 -0800

    Factor out fish_prompt into functions.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
-set BROWSER chrome
-set EDITOR vim
+set -U BROWSER chrome
+set -U EDITOR vim
 
 # Load additional configurations
 set FISH_CONFIG $HOME/.config/fish/
@@ -7,25 +7,3 @@ set FISH_CONFIG $HOME/.config/fish/
 
 # Load private configurations
 . $HOME/.private.fish
-
-# git
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# prompt
-function fish_prompt
-  set last_status $status
-
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
-
-  printf '%s ' (__fish_git_prompt)
-
-  set_color normal
-end
