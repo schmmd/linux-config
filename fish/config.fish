f87d1abfe1c25d821ca4f8be076a32fb0f039998
Michael Schmitz <michael@schmitztech.com>
2014-12-23 11:13:26 -0800

    Add fish configuration.

---
+++ fish/config.fish
@@ -0,0 +1,31 @@
+set BROWSER chrome
+set EDITOR vim
+
+# Load additional configurations
+set FISH_CONFIG $HOME/.config/fish/
+. $FISH_CONFIG/autojump.fish
+
+# Load private configurations
+. $HOME/.private.fish
+
+# git
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# prompt
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
+end
